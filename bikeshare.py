0b0d5b4a5c47d32378638634b765f41b9ae3681b
MajedTam <majed.tami@hotmail.com>
2021-09-23 23:54:42 +0300

    added a description in the main module

--- bikeshare.py
+++ bikeshare.py
@@ -190,7 +190,7 @@ def main():
             print(tabulate(df.iloc[np.arange(0+i,5+i)], headers ="keys"))
             i+=5
             rawData = input('\nWould you like to see raw data? Enter yes or no.\n')
-        
+        #ask the user if he wants to restart the script
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
